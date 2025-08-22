77eda8319fef817cd43d92e9b049a4e0fa51b8a3
Anshil Chotara <anshilchotara@gmail.com>
2025-08-22 14:50:36 +0530

    Add files via upload

---
+++ .NET/CategoryController.cs
@@ -0,0 +1,116 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    // Darshan university 
+
+    public class CategoryController : ControllerBase
+    {
+        #region Configuration Fields 
+        private readonly HrmanagementContext _context;
+
+        public CategoryController(HrmanagementContext context)
+        {
+            _context = context;
+        }
+        #endregion
+
+        #region GET All Detaild 
+        [HttpGet]
+        public async Task<ActionResult<List<Category>>> GetAllDetaild()
+        {
+            var categories = await _context.Categories.ToListAsync();
+            return Ok(categories);
+        }
+        #endregion
+
+        #region DeleteById
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteByID(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+        #endregion
+
+        #region GetByCategoryID
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByCategoryID(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(category);
+        }
+        #endregion
+
+        #region Insert Category
+        [HttpPost]
+        public async Task<IActionResult> InsertCategory([FromBody] Category category)
+        {
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+        #endregion
+
+        #region Update Category
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCategory(int id, [FromBody] Category category)
+        {
+            var existingCategory = await _context.Categories.FindAsync(id);
+            if (existingCategory == null)
+            {
+                return NotFound();
+            }
+
+            existingCategory.CategoryName = category.CategoryName;
+            existingCategory.Description = category.Description;
+            existingCategory.CreatedAt = category.CreatedAt;
+            existingCategory.ModifiedDate = category.ModifiedDate;
+
+            _context.Categories.Update(existingCategory);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+        #endregion  
+        //#region Filter Category
+        //[HttpGet("Filter")]
+        //public async Task<IActionResult> DataFilter(
+        //    [FromQuery] int? CategoryId,
+        //    [FromQuery] string? Description)
+        //{
+        //    var query = _context.Categories.AsQueryable();
+
+        //    if (CategoryId.HasValue)
+        //    {
+        //        query = query.Where(e => e.CategoryId == CategoryId.Value);
+        //    }
+
+        //    if (!string.IsNullOrEmpty(Description))
+        //    {
+        //        query = query.Where(e => e.Description.Contains(Description));
+        //    }
+
+        //    var result = await query.ToListAsync();
+        //    return Ok(result);
+        //}
+        //#endregion
+    }
+}
---
+++ .NET/DepartmentController.cs
@@ -0,0 +1,102 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+
+        #region Configuration Fields 
+        private readonly HrmanagementContext _context;
+        public DepartmentController(HrmanagementContext context)
+        {
+            _context = context;
+        }
+        #endregion
+
+        #region GetAllDetaild
+        [HttpGet]
+        public async Task<ActionResult<List<Department>>> GetAllDetaild()
+        {
+            var departments = await _context.Departments.ToListAsync();
+            return Ok(departments);
+        }
+        #endregion
+
+        #region GetByDepartmentID
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByDepartmentID(int id)
+        {
+
+            var Department = await _context.Departments.FindAsync(id);
+            if (Department == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(Department);
+
+
+        }
+        #endregion
+
+        #region DeleteById
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteByID(int id)
+        {
+            var department = await _context.Departments.FindAsync(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+            _context.Departments.Remove(department);
+           await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        #endregion
+
+        #region Insert Department
+        [HttpPost]
+        public async Task<IActionResult> InsertDepartment([FromBody] Department department)
+        {
+
+            _context.Departments.Add(department);
+            await _context.SaveChangesAsync();
+            return NoContent();
+
+
+        }
+        #endregion
+
+
+        #region Update Department
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateDepartment(int id, [FromBody] Department department)
+        {
+
+            var existingDepartment = await _context.Departments.FindAsync(id);
+            if (existingDepartment == null)
+            {
+                return NotFound();
+            }
+
+            existingDepartment.DeptName = department.DeptName;
+            existingDepartment.Description = department.Description;
+            existingDepartment.CreatedAt = department.CreatedAt;
+            existingDepartment.ModifiedDate = department.ModifiedDate;
+
+            _context.Departments.Update(existingDepartment);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+
+        }
+        #endregion
+
+    }
+}
